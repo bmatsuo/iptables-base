2839e80e4b604c25d708fab9916e39dbeaef5941
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-05-23 09:02:51 +0000

    add rule for a irc bouncer (znc)

--- iptables-init.sh
+++ iptables-init.sh
@@ -19,14 +19,18 @@ $IPTABLES -t mangle -X
 
 # inbound connections
 
-$IPTABLES   -P INPUT    DROP # drop unmatched(?)
-$IPTABLES   -A INPUT    -i lo                                           -j ACCEPT   # loopback
-$IPTABLES   -A INPUT    -m conntrack    --ctstate ESTABLISHED,RELATED   -j ACCEPT   # established sessions
-$IPTABLES   -A INPUT    -p tcp          --dport 22                      -j ACCEPT   # ssh
-$IPTABLES   -A INPUT    -p tcp          --dport 80                      -j ACCEPT   # http
-$IPTABLES   -A INPUT    -p tcp          --dport 443                     -j ACCEPT   # https
-$IPTABLES   -A INPUT    -m limit        --limit 5/min                   -j LOG \
-                        --log-prefix "$IPTABLES_DROP_LOG_PREFIX"    --log-level 7
+$IPTABLES -P INPUT DROP # drop unmatched(?)
+
+$IPTABLES -A INPUT -i lo                                      -j ACCEPT   # loopback
+$IPTABLES -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT   # established sessions
+
+$IPTABLES -A INPUT -p tcp --dport 22   -j ACCEPT   # ssh
+$IPTABLES -A INPUT -p tcp --dport 80   -j ACCEPT   # http
+$IPTABLES -A INPUT -p tcp --dport 443  -j ACCEPT   # https
+$IPTABLES -A INPUT -p tcp --dport 6667 -j ACCEPT   # znc
+
+$IPTABLES -A INPUT -m limit --limit 5/min -j LOG \
+    --log-prefix "$IPTABLES_DROP_LOG_PREFIX" --log-level 7
 
 # output configuration to stdout
 
